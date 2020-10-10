0ec101b5c4ef6b358864cc8fa4e6c737bcd645f1
Will Adams <bushblade@gmail.com>
2020-10-10 09:21:57 +0100

    slicemasters

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,11 +1,82 @@
 import React from 'react'
+import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
-function SliceMastersPage() {
+const SliceMasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`
+
+const SliceMasterStyle = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`
+
+function SliceMastersPage({ data: { slicemasters } }) {
   return (
     <>
-      <p>Hey I'm the slicemasters page</p>
+      <SliceMasterGrid>
+        {slicemasters.nodes.map((person) => (
+          <SliceMasterStyle>
+            <Link key={person.id} to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className='mark'>{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className='description'>{person.description}</p>
+          </SliceMasterStyle>
+        ))}
+      </SliceMasterGrid>
     </>
   )
 }
 
 export default SliceMastersPage
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        id
+        name
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        slug {
+          current
+        }
+      }
+    }
+  }
+`
