ec69b3be37ecdc52239fb1c400dec8a2e3bd9bfb
Will Adams <bushblade@gmail.com>
2020-11-07 08:49:16 +0000

    finish up serverless function

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,30 @@
 const nodemailer = require('nodemailer')
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+    <h2>Your Recent Order for ${total}</h2>
+    <p>Please start walking over, we will have your order ready in the next 20 minutes.</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `
+        <li>
+          <img src="${item.thumbnail}" alt="${item.name}"/>
+          ${item.size} ${item.name} ${item.price}
+        </li>
+      `
+        )
+        .join('')}
+    </ul>
+    <p>Your total is <strong>${total}</strong> due at pickup</p>
+    <style>
+      ul {
+        list-style: none;
+      }
+    </style>
+  </div>`
+}
+
 // create a transport for nodemailer
 
 const transporter = nodemailer.createTransport({
@@ -12,19 +37,37 @@ const transporter = nodemailer.createTransport({
 })
 
 exports.handler = async (event, context) => {
-  // Test send an email
+  const body = JSON.parse(event.body)
+  console.log(body)
+  // validate data
+  const requiredFields = ['email', 'name', 'order']
+  for (const field of requiredFields) {
+    console.log(`checking field exists for ${field}`)
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      }
+    }
+  }
+
+  // send email
+
+  // send the success or error message
 
   const info = await transporter.sendMail({
     from: `Slick's Slices <slick@example.com>`,
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New Order!',
-    html: `<p>Your new Pizza order is here!</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   })
 
   console.log(info)
 
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   }
 }
