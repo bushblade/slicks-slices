326496eaf03edbd5bf70ea5ceef644cec242f191
Will Adams <bushblade@gmail.com>
2020-11-18 08:17:23 +0000

    store page settings

---
+++ sanity/schemas/storeSettings.js
@@ -0,0 +1,31 @@
+// react component for icon
+import { MdStore as icon } from 'react-icons/md'
+
+export default {
+  // computer name
+  name: 'storeSettings',
+  // visible title
+  title: 'Settings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Store Name',
+      type: 'string',
+      description: 'Name of the Store',
+    },
+    {
+      name: 'slicemasters',
+      title: 'Slicemasters Currently Slicing',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'person' }] }],
+    },
+    {
+      name: 'hotSlices',
+      title: 'Hot Slices available in the case',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'pizza' }] }],
+    },
+  ],
+}
